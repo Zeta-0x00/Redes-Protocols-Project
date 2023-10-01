4553a18e597cc3fe0dbe3907dd8f9741330f117a
Adrian Molina <adrmr26@gmail.com>
2023-09-30 18:05:08 -0600

    más cambios

--- modules/stopnwait.py
+++ modules/stopnwait.py
@@ -5,10 +5,33 @@ import random
 import time
 from frame import Packet, Frame
 from typing import Literal
-from timer import Timer
 from threading import Thread
 import signal
 
+class Timer(object):
+    TIMER_STOP = -1
+    def __init__(self, duration)-> None:
+        self.inicio = self.TIMER_STOP
+        self.duracion = duration
+
+    def start(self)-> None:
+        if self.inicio == self.TIMER_STOP:
+            self.inicio = time.time()
+
+    def stop(self)-> None:
+        if self.inicio != self.TIMER_STOP:
+            self.inicio = self.TIMER_STOP
+
+    def running(self)-> bool:
+        return self.inicio != self.TIMER_STOP
+
+    def timeout(self)-> bool:
+        if not self.running():
+            return False
+        else:
+            return time.time() - self.inicio >= self.duracion
+        
+        
 def handler(signum, frame)-> None:
 	print('good bye')
 	exit(0)
